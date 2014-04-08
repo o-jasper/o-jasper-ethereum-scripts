80fee251249fbe06fd062a479e63f8b1fb7cff53
Jasper den Ouden <o.jasper@gmail.com>
2014-04-08 15:18:32 +0200

    Slimming the test

--- param-select/test/test_iter_tree.py
+++ param-select/test/test_iter_tree.py
@@ -8,29 +8,19 @@ sys.path.append(py_path)
 
 from iter_tree import IterTree
 
-class SpecificTestIt():
+class SpecificTest():
     """Tests specific input.
     """
     it = IterTree([1, 2, [[3, 4, 5], [6, 7, 8]], 9, 10])
 
-    def choice_check(self, opts, n=None):
-        if n is None:
-            i = randrange(len(opts))
-            return self.it.next_choice(i) == opts[i]
-        else:
-            return self.it.next_choice(randrange(n)) == opts
     def only(self, result, n=100):
         return self.it.next_choice(randrange(n)) == result
 
     def run(self):
-        assert self.it.cur == 1 and self.only(2)
-
         i = randrange(2)
+        assert self.it.cur == 1 and self.only(2)
         assert self.it.next_choice(i) == [3,6][i]
-        if i == 0:
-            assert self.only(4) and self.only(5)
-        else:
-            assert self.only(7) and self.only(8)
+        assert self.only([4,7][i]) and self.only([5,8][i])
         assert self.only(9) and self.only(10)
 
-SpecificTestIt().run()
+SpecificTest().run()
