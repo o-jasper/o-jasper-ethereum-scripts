c6b7baf01b140b9bbeffbf4976fd7b9cff2beeb8
Jasper den Ouden <o.jasper@gmail.com>
2014-04-18 18:11:55 +0200

    Added base gtk parameter selector class

--- transaction-firewall/lib/gui/ParamKindsGtk.py
+++ transaction-firewall/lib/gui/ParamKindsGtk.py
@@ -19,43 +19,44 @@ def pack_end(list, into=None):
         into.pack_end(el, True, True, 0)
     return into
 
-class ParamListGtk:
-    """Just for ParamList"""
+class ParamGtkBase:
     hidden = True
-    
+
+    def __init__(self, it, parentinfo=None):
+        self.it = it
+        self.parentinfo = parentinfo
+
+    def hide(self):
+        if not self.hidden:
+            self.gtk_el.hide()
+            self.hidden = True
+
+    def show(self, show=True):
+        if not show:
+            self.hide()
+        elif self.hidden:
+            self.gtk_el.show()
+            self.hidden = False
+
+class ParamListGtk(ParamGtkBase):
+    """Just for ParamList"""
     def __init__(self, top=None, max=None, parentinfo=None):
         self.top = top
-        self.vbox = vbox([top])
+        self.gtk_el = vbox([top])
         # self.parentinfo = parentinfo
     def gtk_set(self, value):
         self.top.gtk_set(value)  # It does the parent stuff.
 
     def gtk_add(self, added):
-        self.vbox.pack_end(added, True, True, 0)
+        self.gtk_el.pack_end(added, True, True, 0)
 
-    def hide(self):
-        if not self.hidden:
-            self.vbox.hide()
-            self.hidden = True
-
-    def show(self, show=True):
-        if not show:
-            if self.hidden:
-                self.vbox.show()
-                self.hidden = False
-        else:
-            self.hide()
-
-class ParamNumberGtk():
+class ParamNumberGtk(ParamGtkBase):
     """For next to any number param kind"""
-    hidden = True
-    
     def __init__(self, it, parentinfo=None, digits=2, size=600, climb_rate=None):
-        self.min = it.min
-        self.max = it.max
         self.parentinfo = parentinfo
-
-        fx,tx = if_none(0, self.min),if_none(0, self.max)
+        self.it = it
+        
+        fx,tx = if_none(0, it.min),if_none(1, it.max)
 
         climb_rate = if_none((tx - fx)/100.0, climb_rate)
         self.adjustment = Gtk.Adjustment(value=if_none(fx, it.default), lower=fx, upper=tx,
@@ -63,33 +64,17 @@ class ParamNumberGtk():
         
         self.spinner = Gtk.SpinButton(adjustment=self.adjustment,
                                       climb_rate=climb_rate, digits=digits)
-        self.spinner.set_range(if_none(0, self.min), if_none(1, self.max))
+        self.spinner.set_range(fx, tx)
         self.spinner.name = it.name
 
         self.hscrollbar = Gtk.HScrollbar(self.adjustment)
         self.hscrollbar.set_min_slider_size(size)
 
         self.adjustment.connect('value-changed', self.adjustment_value_changed)
-        self.spinner.connect('change-value', self.spinner_change_value)
+#        self.spinner.connect('change-value', self.spinner_change_value)
 
-        self.hbox = pack_start([Gtk.Label(it.name + ":"), self.spinner,self.hscrollbar])
+        self.gtk_el = pack_start([Gtk.Label(it.name + ":"), self.spinner,self.hscrollbar])
 
-    @property
-    def gtk_el(self):
-        return self.hbox
-
-    def hide(self):
-        if not self.hidden:
-            self.hbox.hide()
-            self.hidden = True
-
-    def show(self, show=True):
-        if not show:
-            self.hide()
-        elif self.hidden:
-            self.hbox.show()
-            self.hidden = False
-    
     def adjustment_value_changed(self, adjustment):
         self.value_changed(adjustment.get_value())
         return 0
@@ -100,7 +85,8 @@ class ParamNumberGtk():
             parent.update(i, value)
 
     def spinner_change_value(self, sb, st):
-        print(st)
+        print('---',st)
+        print(Gtk.GTK_SCROLL_STEP_UP)
         adj = sb.get_adjustment() 
         val = adj.get_value()
         if st == Gtk.SCROLL_STEP_UP: #TODO cant figure names.. should be Gtk.SCROLL_STEP_UP
@@ -123,15 +109,7 @@ class ParamNumberGtk():
     def get_value(self):
         return self.adjustment.get_value()
 
-class ParamStringGtk():
-
-    hidden = True
-
-    @property
-    def gtk_el(self):
-        return self.hbox
-
-    
+class ParamStringGtk(ParamGtkBase):
     def __init__(self, it, parentinfo=None):
         self.entry = Gtk.Entry()
-        self.hbox = pack_start([Gtk.Label(it.name + ":"), self.entry])
+        self.gtk_el = pack_start([Gtk.Label(it.name + ":"), self.entry])
