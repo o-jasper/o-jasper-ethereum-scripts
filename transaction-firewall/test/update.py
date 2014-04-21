c6320c5f655970c5604ec84724704e6cab98b3f9
Jasper den Ouden <o.jasper@gmail.com>
2014-04-21 22:45:26 +0200

    very limited update test

---
+++ transaction-firewall/test/update.py
@@ -0,0 +1,17 @@
+from random import randrange, random
+import choose
+
+class TestExample(choose.TestBranch):
+
+    def test_update(self):
+        i,r = self.gi(), random()
+        self.ps.update(i, r)
+        val = self.ps.values[str(i[1:])]
+        assert val[0] is i
+        assert val[1] == r
+
+    def test(self, n=100):
+        for ignored in range(n):
+            self.test_update()
+
+TestExample().test()
