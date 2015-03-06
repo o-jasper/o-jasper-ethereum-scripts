ed75058d0ecada4592fea66958189b35899555c3
Jasper den Ouden <o.jasper@gmail.com>
2015-03-06 02:25:43 +0100

    got `cases.py` and `assurance_entity.se` to PoC8

---
+++ assurance_entity/cases.py
@@ -0,0 +1,144 @@
+import pyethereum
+from random import randrange
+u = pyethereum.utils
+t = pyethereum.tester
+
+def i(str):
+    s,f = 0, 1
+    for j in range(len(str)):
+        s += f*ord(str[len(str)-j-1])
+        f *= 256
+    for j in range(32 - len(str)): # Right pad instead of left.
+        s *= 256;
+    return s
+
+def stri(j):
+    s=""
+    while j > 0:
+        s += chr(j%256)
+        j /=256
+    return "".join(reversed(s))
+
+def any_key(disallow=None):
+    if not disallow:
+        disallow = []
+    elif not isinstance(disallow, list):
+        disallow = [disallow]
+    i = randrange(len(t.keys) - len(disallow))
+    while t.keys[i] in disallow:
+        i += 1
+    assert i < len(t.keys)
+    return t.keys[i]
+
+s = None
+c = None
+
+def reset():
+    global c, s, end_time
+    s = t.state()
+    c = s.abi_contract('assurance_ent.se', t.k0)
+
+
+def check(a, n):  # TODO this would be better with 'stateless call'
+    assert c.balance() == a
+    assert c.cnt() == n
+
+    # Check it isnt overwriting permanents
+    assert hex(c.creator())[2:-1] == t.a0
+    assert hex(c.recipient())[2:-1] == t.a0
+    assert c.endtime() == end_time
+    assert c.min() == 20000
+    assert c.max() == 30000
+
+    assert c.refund(sender=any_key(t.k0)) == i("only creator/self")
+    assert c.initialize(t.a2, t.a2, s.block.timestamp  + 600, 24000, 30000) == \
+           i("already initialized")
+
+def check_blank():
+    assert c.balance() == 0
+    assert c.recipient() == 0
+    assert c.endtime() == 0
+    assert c.min() == 0
+    assert c.max() == 0
+    assert c.cnt() == 0
+    assert hex(c.creator())[2:-1] == t.a0
+    assert c.pay_i(0, sender=t.k0, value=randrange(46364)) == i("not ready")
+    assert c.initialize(t.a2, t.a2, s.block.timestamp  + 600, 24000, 30000, \
+                        sender=any_key(t.k0)) == \
+        i("not creator")
+
+befores = None
+
+def scenario_init():
+    global end_time, befores
+    befores = {}
+    reset()
+    end_time = s.block.timestamp  + 200
+    check_blank()
+    assert c.initialize(t.a0, t.a0, end_time, 20000, 30000) == i("initialized")
+    check(0, 0)
+
+def pay(k, a, must_be_paid):
+    global befores
+    sender = any_key()
+    addr = u.privtoaddr(sender)
+#    befores[addr] = befores[addr] or s.block.balance(int("0x" + addr))
+    
+    got =  c.pay_i(k, sender=sender, value=a)
+    if got == i("index paid"):
+        assert hex(c.fund_addr_i(k))[2:-1] == sender
+    elif got == i("paid"):
+        return
+    assert must_be_paid
+    if k > 2**64:
+        assert got == i("unrealistic")
+    assert got == i("hit max") and c.balance() + ca > c.max()
+
+def scenario_dont_reach():
+    scenario_init()
+    check(0,0)
+    n, a  = randrange(10), 0
+    for j in range(n):  # Pay, but dont reach.
+        ca = randrange(19999/n)
+        pay(0, ca, True)
+        a += ca
+        check(a, j + 1)
+    return a, n
+
+def check_refund():
+    0
+#    for addr in t.accounts: #... they're paying for gas too.
+#        if addr != t.a0:
+#            assert s.block.balance(addr) == befores[addr]
+
+def scenario_underfunded():
+    a, n = scenario_dont_reach()
+    while s.block.timestamp < end_time:  # Reach the time.
+        s.mine()
+    check(a, n)
+    assert c.finish(sender=any_key()) == i("underfunded")
+    check_blank()
+    check_refund()
+
+def scenario_funded():
+    a,n = scenario_dont_reach()
+    while c.balance() < c.min(): # Go to threshhold.
+        ca = randrange(5000)
+        pay(0, ca, False)
+        a += ca
+        n += 1
+        check(a,n)
+    while s.block.timestamp < end_time:  # Reach the time.
+        s.mine()
+    check(a, n)
+    assert c.finish(sender=any_key()) == i("funded")
+    assert c.balance() == 0
+
+def scenario_refunded():
+    a, n = scenario_dont_reach()
+    assert c.refund(sender=t.k0) == i("manual refund")
+    check_refund()
+
+scenario_underfunded()
+scenario_funded()
+scenario_refunded()
---
+++ assurance_entity/readme.md
@@ -0,0 +1,68 @@
+**NOTE** still needs more porting to PoC8, particularly, the web stuff.
+
+# Assurance Ethereum entity. 
+
+Assurance Ethereum entity.
+
+* Creator creates, then initiates, sets it up, sets recipient, sets block
+  number it will have to succeed/fail, sets minimum success amount.
+
+* Anyone can help fund.
+
+* Before deadline, creator can refund.
+
+* Anything occuring after the deadline sends the funds to the project if
+  success, and refunds otherwise.
+
+### Potential expansions
+Some of these might be doable by simply connecting to other contracts. But
+that might not be gas-efficient.
+
+* Stages; if funded, it goes to stage 1, stage 2 needs something to be
+  triggered. That might mean more funding, votes from existing funders or
+  just keeping the same funding, but allowing some funders to cut their losses
+  if they lose faith.
+  
+  There could be multiple stages this way.
+
+* Dividents: paying for stuff like stocks. Need some rules about how much to do 
+  dividents. 
+  
+  Not *particularly* happy with use of stocks currently, but then
+  oculus-like events suck too.
+
+## Tests
+Just one that does a case test. 
+
+Do **not** think it has high enough certainty for use. 
+
+It is bound to need quite a lot of rewriting towards better contract writing.
+Like formalizing the commands, efficiency, getting rid of the `refund` variable
+being used as it is now..
+
+Otherwise for security, need to try think of ways to break it, analyse the code,
+and throw more random stuff at it in tests.
+
+# TODO (note: only if i take it further)
+
+* Go PoC7..
+  + Get rid of that awful `refund` variable doing control flow.
+  + Some static checks.
+
+* Ensure cant attack by making many tiny contributions, *later* costing too much gas.
+  (contribution minimum)
+
+* The refund button should hide if you cannot control it. Also, refunding after the
+  whole thing.
+
+* More thurrough case test.
+  + All logical paths.
+  + Fuzz-test?
+  + Reading well/annotating?
+
+* It is not pretty enough.
+
+* Show everything, and safety-off switch. NOTE: the safeties are merely some extra
+  checks, do not imply safety.
+
+* Paying and refunding uses same interface, renaming stuff.. I dont like it.
